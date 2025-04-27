ad6d2a563db97b97117a69da2d04df71c4113c9f
Carlos Hdz <carloshdz@Mac-mini-de-Carlos.local>
2025-04-26 23:28:10 -0600

    :bricks: change vscode settings to root folder

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
 import { defineConfig } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
@@ -13,30 +14,36 @@ export default defineConfig([
     plugins: {
       js,
       react: pluginReact,
+      prettier: pluginPrettier,
     },
     settings: {
       react: {
         version: 'detect',
       },
     },
-    extends: [js.configs.recommended, pluginReact.configs.flat.recommended, eslintConfigPrettier],
+    extends: [
+      js.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      eslintConfigPrettier, // Apaga reglas que chocan con Prettier
+    ],
     rules: {
+      // Prettier como regla de error
+      'prettier/prettier': 'error',
+
       // JavaScript
-      'no-undef': 'error', // Check undefined variables
-      'no-console': 'error', // Don't Allow console.logs
-      'prefer-const': 'warn', // Use const whenever possible
-      'no-var': 'error', // Don't allow var
-      eqeqeq: ['error', 'always'], // Use === always
-      semi: ['error', 'always'], // Force semi colon
-      quotes: ['error', 'single'], // Force simple quotes
+      'no-undef': 'error',
+      'no-console': 'error',
+      'prefer-const': 'warn',
+      'no-var': 'error',
+      eqeqeq: ['error', 'always'],
 
       // React
-      'react/react-in-jsx-scope': 'off', // Is not necessary to import React from v17+
-      'react/prop-types': 'off', // Don't user PropTypes
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
       'react/jsx-uses-react': 'off',
-      'react/jsx-uses-vars': 'error', // Show "variable is defined but never used"
-      'react/jsx-no-duplicate-props': 'error', // Avoid duplicated props
-      'react/self-closing-comp': 'warn', // Force auto-close componente whenever needed
+      'react/jsx-uses-vars': 'error',
+      'react/jsx-no-duplicate-props': 'error',
+      'react/self-closing-comp': 'warn',
     },
   },
 ]);
